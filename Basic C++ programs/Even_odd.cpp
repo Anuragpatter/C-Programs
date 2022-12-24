3877e9b5748bd89b993d1a1a84e57097656ea6dc
Anurag patter <93168851+Anuragpatter@users.noreply.github.com>
2022-12-25 01:06:27 +0530

    Add files via upload

---
+++ Basic C++ programs/Even_odd.cpp
@@ -0,0 +1,14 @@
+// CODE FOR EVEN OR ODD NUMBERS:
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter the required number: ";
+    cin>>num;
+    if(num%2==0)                      
+    cout<<num <<" is a even number";
+    else
+    cout<<num <<" is a odd number";
+    return 0;
+}  
